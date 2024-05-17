12ce1a89122ce730a150cd025166d969b96da1b2
Harshad Tembhe <Harshadtembhe1234@gmail.com>
2024-05-17 17:19:09 +0530

    Add a new Contact to Address Booke.

---
+++ src/AddressBook.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<Contact> contacts;
+
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+    public void displayContacts() {
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+    }
+}
